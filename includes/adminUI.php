77d1e6893e50fd04609e9c532594e3d8be9f1a59
olddriverwang <59304676+RSSYLY@users.noreply.github.com>
2021-01-26 20:46:10 +0800

    add cdn

--- includes/adminUI.php
+++ includes/adminUI.php
@@ -7,26 +7,40 @@
 	}	
 	// Add a new variable and sets the default values in red
 	function set_rssgitcdn_options(){		
-		add_option('rssgitcdn_cdnurl','raw.sevencdn.com');
+		add_option('rssgitcdn_cdnurl_cdn5telesco','cdn5.telesco.pe');
+		add_option('rssgitcdn_cdnurl_cdn1telesco','cdn1.telesco.pe');
+	    add_option('rssgitcdn_cdnurl','raw.sevencdn.com');
 	}	
 	// delete Ray_color key 
 	function unset_rssgitcdn_options(){ 
+		delete_option('rssgitcdn_cdnurl_cdn1telesco');
+		delete_option('rssgitcdn_cdnurl_cdn5telesco');
 		delete_option('rssgitcdn_cdnurl');
 	}		
 	// The plugin Settings page content
 	function rssgitcdn_options_page(){ ?>
-		<div class="wrap">        	
-            <h2>Redsunset Github CDN 设置</h2>
+
+
+
+<div class="wrap">        	
+            <h2>Redsunset CDN 设置</h2>
             <?php update_rssgitcdn_options();	// update Ray_color?>
-<form method="post">Github RAW CDN: <input class="regular-text code" type="text" name="rssgitcdn_cdnurl" value="<?php echo get_option('rssgitcdn_cdnurl');?>"/>
+<form method="post">
+	Github Raw<input class="regular-text code" type="text" name="rssgitcdn_cdnurl" value="<?php echo get_option('rssgitcdn_cdnurl');?>"/><br>
+	cdn1telesco<input class="regular-text code" type="text" name="rssgitcdn_cdnurl_cdn1telesco" value="<?php echo get_option('rssgitcdn_cdnurl_cdn1telesco');?>"/><br>
+	cdn5telesco<input class="regular-text code" type="text" name="rssgitcdn_cdnurl_cdn5telesco" value="<?php echo get_option('rssgitcdn_cdnurl_cdn5telesco');?>"/><br>
 <input class="button button-primary" type="submit" name="submit" value="保存设置"/>
             </form>
-        </div><?php
-            $txt_file=file("https://raw.fastgit.org/RSSYLY/redsunset-wp-gitrawcdn/main/cdnresource.txt");
-            foreach($txt_file as $value){
-                echo $value."<br />";
-            }
-        ?><?php
+</div>
+
+
+
+<p>△使用说明
+目前可用的Github CDN 
+更新于 2020.12.13
+raw.sevencdn.com
+raw.fastgit.org</p>
+<?php
 	}	
 	// update Ray_color option
 	function update_rssgitcdn_options(){
@@ -35,6 +49,16 @@
 			if($_POST['rssgitcdn_cdnurl']){
 				//echo "die";exit;
 				update_option('rssgitcdn_cdnurl',$_POST['rssgitcdn_cdnurl']);
+				$flag = true;
+			}
+			if($_POST['rssgitcdn_cdnurl_cdn1telesco']){
+				//echo "die";exit;
+				update_option('rssgitcdn_cdnurl_cdn1telesco',$_POST['rssgitcdn_cdnurl_cdn1telesco']);
+				$flag = true;
+			}
+			if($_POST['rssgitcdn_cdnurl_cdn5telesco']){
+				//echo "die";exit;
+				update_option('rssgitcdn_cdnurl_cdn5telesco',$_POST['rssgitcdn_cdnurl_cdn5telesco']);
 				$flag = true;
 			}
 			if($flag){
